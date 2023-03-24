fbc45235ab2ef0eef91d39a627fc0dc1ffb46b75
Tyler Maksymiw <tylermcodes@gmail.com>
2023-03-23 21:02:35 -0400

    Update company card component

--- client/src/Components/CompanyCard.jsx
+++ client/src/Components/CompanyCard.jsx
@@ -3,27 +3,19 @@ import "../Styles/Companies.scss"
 function CompanyCard(){
     return(
         <div className="company-card">
-            <div className="inner-card flex-column flex-column--center"> 
+            <div className="company-info">
                 <div>
-                    <h3>ABC Inc</h3>
-                    <p>Launch & Deliver - Active</p>
+                    <h3>Company ABC</h3>
+                    <p className="program">Launch & Deliver</p>
                 </div>
-              
-                <div>
-                    <div className="circle flex-column flex-column--center">
-                        <h2>5</h2>
-                    </div>
-                    <span>Coaching Hours</span>
-                </div>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
-            </div>
-            <div className="cma-container">
-                <h3>CMA Summary</h3>
-                <div className="cma cma--team flex-row flex-row--space">
-                    <p>Market</p>
-                    <p>3</p>
+                <div className="status">
+                    <p>Active</p>
                 </div>
             </div>
+            {/* <div className="flex-column flex-column--center">
+                <h2>5</h2>
+                <p>Coaching Hours</p>
+            </div> */}
         </div>
     )
 }
