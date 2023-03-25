692e476089ce088070227259e9591e390f91913f
Tyler Maksymiw <tylermcodes@gmail.com>
2023-03-25 15:56:45 -0400

    Update options component

--- client/src/Components/Options.jsx
+++ client/src/Components/Options.jsx
@@ -1,12 +1,13 @@
 import useAuth from "../hooks/useAuth"
+import "../Styles/Options.scss"
 
 function Options() {
     const { auth } = useAuth()
 
     return(
-        <section className="dashboard">
+        <section id="options" className="dashboard">
             <h2>Account Information</h2>
-            <div className="gap--4 flex-row">
+            <div className="gap--2 flex-row">
                 <div>
                     <label htmlFor="fName">First Name</label>
                     <input 
@@ -25,13 +26,43 @@ function Options() {
                 </div>
             </div>
             <div>
-                <label htmlFor="">Email</label>
+                <label htmlFor="email">Email</label>
                 <input 
+                    disabled
+                    className="not-allowed"
                     type="text" 
-                    name="" 
-                    id="" 
+                    name="email" 
+                    placeholder={auth.userName}
                 />
             </div>
+            <div>
+                <label htmlFor="current-password">Email</label>
+                <input 
+                    type="password" 
+                    name="current-password" 
+                    placeholder="Enter Current Password"
+                />
+                <p>Enter your current password to update your account settings</p>
+            </div>
+            <div className="flex-row gap--2">
+                <div>
+                    <label htmlFor="new-password">New Password</label>
+                    <input
+                        type="text"
+                        name="new-password"
+                        placeholder="New Password"
+                    />
+                </div>
+                <div>
+                    <label htmlFor="confirm-password">Confirm New Password</label>
+                    <input
+                        type="text"
+                        name="confirm-password"
+                        placeholder="Confirm New Password"
+                    />
+                </div>
+            </div>
+           
         
         </section>
     )
