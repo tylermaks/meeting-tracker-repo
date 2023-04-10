8beb94418247e91178e65e463a7505259fb4f154
Tyler Maksymiw <tylermcodes@gmail.com>
2023-04-10 09:30:58 -0400

    Update netlify server function

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,10 +1,11 @@
 const express = require('express')
-const credentials = require('./middleware/credentials')
+const credentials = require('../../server/middleware/credentials')
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
-const corsOptionsDelegate = require('./config/cors')
+const corsOptionsDelegate = require('../../server/config/cors')
 const app = express()
-const verifyJWT = require('./middleware/verifyJWT')
+const verifyJWT = require('../../server/middleware/verifyJWT')
+const serverless = require('serverless-http');
 
 //SETUP
 app.use(credentials)
@@ -14,22 +15,14 @@ app.use(express.json())
 app.use(cookieParser())
 
 //ROUTES
-app.use('/.netlify/functions/auth', require('./routes/api/auth'))
-app.use('/.netlify/functions/refresh', require('./routes/api/refresh'))
-app.use(verifyJWT)
-app.use('/.netlify/functions/meetingList', require('./routes/api/meetingList'))
-app.use('/.netlify/functions/logout', require('./routes/api/logout'))
-app.use('/.netlify/functions/meeting', require('./routes/api/meeting'))
-app.use('/.netlify/functions/companies', require("./routes/api/companies"))
-app.use('/.netlify/functions/settings', require('./routes/api/settings'))
-app.use('/.netlify/functions/supportRequests', require('./routes/api/supportRequests'))
+app.use('/auth', require('./auth'))
+// app.use('/.netlify/functions/server/refresh', require('./routes/api/refresh'))
+// app.use(verifyJWT)
+// app.use('/.netlify/functions/server/meetingList', require('./routes/api/meetingList'))
+// app.use('/.netlify/functions/server/logout', require('./routes/api/logout'))
+// app.use('/.netlify/functions/server/meeting', require('./routes/api/meeting'))
+// app.use('/.netlify/functions/server/companies', require("./routes/api/companies"))
+// app.use('/.netlify/functions/server/settings', require('./routes/api/settings'))
+// app.use('/.netlify/functions/server/supportRequests', require('./routes/api/supportRequests'))
 
-exports.handler = async (event, context) => {
-  const PORT = 9000;
-//   const server = app.listen(PORT, () => console.log(`Server is running on Port: ${PORT}`))
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: `Server is running on Port: ${PORT}` }),
-  };
-}
+module.exports.handler = serverless(app);
