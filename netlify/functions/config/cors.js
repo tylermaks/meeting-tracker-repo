3f66f1da18624d70f849712b4338c1259cf4053c
Tyler Maksymiw <tylermcodes@gmail.com>
2023-04-11 11:05:41 -0400

    Update cors.js

--- netlify/functions/config/cors.js
+++ netlify/functions/config/cors.js
@@ -4,7 +4,10 @@ const allowedOrigins = require("./allowedOrigins")
 const corsOptionsDelegate = function (req, callback) {
     let corsOptions;
     if (allowedOrigins.indexOf(req.header('Origin')) !== -1) {
-      corsOptions = { origin: true, 'Access-Control-Allow-Credentials': true} // reflect (enable) the requested origin in the CORS response
+      corsOptions = { 
+        origin: true, 
+        credentials: true,
+        'Access-Control-Allow-Credentials': true} // reflect (enable) the requested origin in the CORS response
     } else {
       corsOptions = { origin: false } // disable CORS for this request
     }
