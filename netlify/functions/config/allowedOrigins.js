89469b972cabcd9ba4aff5fd88648a96bfe3d0a6
Tyler Maksymiw <tylermcodes@gmail.com>
2023-04-11 10:47:54 -0400

    Update allowed origins

--- netlify/functions/config/allowedOrigins.js
+++ netlify/functions/config/allowedOrigins.js
@@ -1,3 +1,7 @@
-const allowedOrigins = [ 'https://golden-pastelito-f5afa8.netlify.app/.netlify/functions', 'https://golden-pastelito-f5afa8.netlify.app/.netlify/functions/auth']
+const allowedOrigins = [
+    'https://golden-pastelito-f5afa8.netlify.app/.netlify/functions',
+    'https://golden-pastelito-f5afa8.netlify.app/.netlify/functions/auth',
+    'https://main--golden-pastelito-f5afa8.netlify.app'
+  ]
 
 module.exports = allowedOrigins
