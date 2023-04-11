d88ab352a93f1d94659cdcfabb5707d2284ae708
Tyler Maksymiw <tylermcodes@gmail.com>
2023-04-11 10:36:56 -0400

    Update server and credentials

--- netlify/functions/middleware/credentials.js
+++ netlify/functions/middleware/credentials.js
@@ -1,24 +1,11 @@
-const allowedOrigins = require("../config/allowedOrigins");
+const allowedOrigins = require("../config/allowedOrigins")
 
-exports.handler = function (event, context, callback) {
-  const origin = event.headers.origin;
+const credentials = (req, res, next) => {
+    const origin = req.headers.origin
+    if(allowedOrigins?.includes(origin)){
+        res.header('Access-Control-Allow-Credentials', true)
+    }
+    next()
+}
 
-  if (allowedOrigins.includes(origin)) {
-    const response = {
-      statusCode: 200,
-      headers: {
-        "Access-Control-Allow-Origin": origin,
-        "Access-Control-Allow-Credentials": true,
-      },
-    };
-    return callback(null, response);
-  }
-
-  const response = {
-    statusCode: 403,
-    body: JSON.stringify({
-      error: "Not allowed to access this resource from this origin.",
-    }),
-  };
-  return callback(null, response);
-};
+module.exports = credentials
