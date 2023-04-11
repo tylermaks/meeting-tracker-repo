1ac195d46493de5ab7670239302a720eeb60ea33
Tyler Maksymiw <tylermcodes@gmail.com>
2023-04-11 08:07:33 -0400

    Update authentication component

--- src/Components/AuthenticationComponent/Authentication.jsx
+++ src/Components/AuthenticationComponent/Authentication.jsx
@@ -1,11 +1,12 @@
 import LoginForm from './LoginForm'
+import logo from "../../Images/logo-white.png"
 import "../../Styles/Authentication/Login.scss"
 import WelcomeModal from './WelcomeModal';
 
 function Authentication(){
     return(
         <section id="login" className="flex-column flex-column--center">
-            <h1>Logo</h1>
+            <img src={logo} alt="Company Logo" />
             <LoginForm/>
             <WelcomeModal />
         </section>
