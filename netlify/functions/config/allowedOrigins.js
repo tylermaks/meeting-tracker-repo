57886e0ca8d7a18b2a39e465c027f028931b2658
Tyler Maksymiw <tylermcodes@gmail.com>
2023-04-11 07:50:18 -0400

    Update allowedOrigins

--- netlify/functions/config/allowedOrigins.js
+++ netlify/functions/config/allowedOrigins.js
@@ -1,3 +1,3 @@
-const allowedOrigins = ['http://127.0.0.1:3000', 'http://localhost:3000','http://127.0.0.1:5000', 'http://localhost:5000', 'https://6434c7391670f157be33a667--golden-pastelito-f5afa8.netlify.app/']
+const allowedOrigins = ['http://127.0.0.1:3000', 'http://localhost:3000','http://127.0.0.1:5000', 'http://localhost:5000', 'https://golden-pastelito-f5afa8.netlify.app/.netlify/functions']
 
 module.exports = allowedOrigins
