a24592116f28e95a8fff783419e5f7463abd6120
Tyler M <76651883+tylermaks@users.noreply.github.com>
2023-04-11 23:03:59 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -13,9 +13,9 @@ const path = require('path');
 app.use(express.static(path.join(__dirname, '..', 'client', 'build')))
 
 // Serve the index.html file as the default page for all other routes
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'))
- })
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'))
+//  })
 
 
 //SETUP
