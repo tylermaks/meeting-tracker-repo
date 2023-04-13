c417fc0dfdd75dc90ca23498b1e5530a888d053a
Tyler Maksymiw <tylermcodes@gmail.com>
2023-04-12 23:59:53 -0400

    Update RequireAuth component

--- client/src/Components/AuthenticationComponent/RequireAuth.js
+++ client/src/Components/AuthenticationComponent/RequireAuth.js
@@ -1,21 +1,17 @@
 import { useLocation, Navigate, Outlet } from "react-router-dom";
-import useAuth from "../../hooks/useAuth";
+import useAuth from "../hooks/useAuth";
 
 const RequireAuth = ({ allowedRoles }) => {
-  const { auth, isLoading } = useAuth();
-  const location = useLocation();
+    const { auth } = useAuth();
+    const location = useLocation();
 
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
-  return allowedRoles.includes(auth?.roles) ? (
-    <Outlet />
-  ) : auth?.username ? (
-    <Navigate to="/unauthorized" state={{ from: location }} replace />
-  ) : (
-    <Navigate to="/" state={{ from: location }} replace />
-  );
-};
+    return (
+        auth?.roles?.find(role => allowedRoles?.includes(role))
+            ? <Outlet />
+            : auth?.user
+                ? <Navigate to="/unauthorized" state={{ from: location }} replace />
+                : <Navigate to="/login" state={{ from: location }} replace />
+    );
+}
 
 export default RequireAuth;
