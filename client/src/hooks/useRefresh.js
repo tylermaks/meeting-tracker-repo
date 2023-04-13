0c17bc02824340e447ef9557d14e4b5f7a5ed71f
Tyler Maksymiw <tylermcodes@gmail.com>
2023-04-13 19:24:56 -0400

    Update useRefresh hook

--- client/src/hooks/useRefresh.js
+++ client/src/hooks/useRefresh.js
@@ -6,18 +6,12 @@ const useRefresh = () => {
 
     const refresh = async () => {
         //GET Requst for refesh token 
-        try{
-            const response = await axios.get('/refresh')
-    
-            console.log(response)
-            
-            const { accessToken, userName, roles, fName, lName } = response.data;
-            setAuth({ roles, userName, fName, lName, accessToken });
-    
-            return response.data.accessToken
-        } catch(err) { 
-            console.error(err)
-        } 
+        const response = await axios.get('/refresh')
+        console.log(response)
+        const { accessToken, userName, roles, fName, lName } = response.data;
+        setAuth({ roles, userName, fName, lName, accessToken });
+
+        return response.data.accessToken
     }
     
     return refresh
